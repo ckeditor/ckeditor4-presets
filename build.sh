cad05eaef65d8f65c659d088b4350ffbbafd8e72
fredck <f.knabben@cksource.com>
2012-11-22 13:18:47 +0100

    Fixed license information.

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Copyright (c) 2003-2012, CKSource - Frederico Knabben. All rights reserved.
-# For licensing, see LICENSE.html or http://ckeditor.com/license
+# For licensing, see http://ckeditor.com/license
 
 # Build CKEditor using the default settings (and build.js)
 
