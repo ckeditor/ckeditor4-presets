5e3e066b314ff22cfc89567dda770853f48e0d6d
Piotrek Reinmar Koszuliński <pkoszulinski@gmail.com>
2012-12-28 11:20:26 +0100

    Add SCAYT and WSC plugins to full build config.

--- presets/full-build-config.js
+++ presets/full-build-config.js
@@ -56,6 +56,7 @@ var CKBUILDER_CONFIG = {
 		removeformat: 1,
 		resize: 1,
 		save: 1,
+		scayt: 1,
 		selectall: 1,
 		showblocks: 1,
 		showborders: 1,
@@ -69,6 +70,7 @@ var CKBUILDER_CONFIG = {
 		templates: 1,
 		toolbar: 1,
 		undo: 1,
+		wsc: 1,
 		wysiwygarea: 1
 	}
 };
