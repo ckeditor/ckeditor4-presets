0f6eca8df498c1f15d0deb58ef719bb5ed7d91f0
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2013-09-13 10:57:07 +0200

    Let's have the version number in lower case.

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 
 # Build CKEditor using the default settings (and build.js)
 
-CKEDITOR_VERSION="4.3 BETA"
+CKEDITOR_VERSION="4.3 beta"
 
 CKBUILDER_VERSION="1.7"
 CKBUILDER_URL="http://download.cksource.com/CKBuilder/$CKBUILDER_VERSION/ckbuilder.jar"
