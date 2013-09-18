8f501da46cf3ccceabef364926c8a0ceb4b9d659
Piotrek Reinmar Koszuliński <pkoszulinski@gmail.com>
2013-09-18 10:43:21 +0200

    Started 4.3 final development.

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 
 # Build CKEditor using the default settings (and build.js)
 
-CKEDITOR_VERSION="4.3 beta"
+CKEDITOR_VERSION="4.3"
 
 CKBUILDER_VERSION="1.7.1"
 CKBUILDER_URL="http://download.cksource.com/CKBuilder/$CKBUILDER_VERSION/ckbuilder.jar"
