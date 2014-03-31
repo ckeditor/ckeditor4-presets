a822df86e92693326c568c8888aa274e41475ca1
Piotrek Reinmar Koszuliński <pkoszulinski@gmail.com>
2014-03-31 10:49:25 +0200

    Changed version to force builder to create separate directories.

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 
 # Build CKEditor using the default settings (and build.js)
 
-CKEDITOR_VERSION="4.4.0"
+CKEDITOR_VERSION="4.4.0-no-wsc"
 
 CKBUILDER_VERSION="1.7.2"
 CKBUILDER_URL="http://download.cksource.com/CKBuilder/$CKBUILDER_VERSION/ckbuilder.jar"
