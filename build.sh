6c1aa2f62213fe525851c92ec3b92a8abb769adb
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2015-12-09 17:07:29 +0100

    Started 4.5.7 development.

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@
 # Move to the script directory.
 cd $(dirname $0)
 
-CKEDITOR_VERSION="4.5.6"
+CKEDITOR_VERSION="4.5.7"
 
 CKBUILDER_VERSION="2.3.0"
 CKBUILDER_URL="http://download.cksource.com/CKBuilder/$CKBUILDER_VERSION/ckbuilder.jar"
