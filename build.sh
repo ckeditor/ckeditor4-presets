c16d59b826128b460afb56df7d4b4d11e895d8b0
Kajetan Litwinowicz <k.litwinowicz@cksource.com>
2018-01-08 09:27:24 +0100

    Updated license url references.

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Copyright (c) 2003-2018, CKSource - Frederico Knabben. All rights reserved.
-# For licensing, see http://ckeditor.com/license
+# For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
 
 # Build CKEditor using the default settings (and build.js)
 
--- presets/full-ckeditor-config.js
+++ presets/full-ckeditor-config.js
@@ -1,6 +1,6 @@
 /**
  * @license Copyright (c) 2003-2018, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see http://ckeditor.com/license
+ * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
  */
 
 CKEDITOR.editorConfig = function( config ) {
